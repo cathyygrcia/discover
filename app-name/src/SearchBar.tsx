9de5af3a09a880fb6ca61011298872fea7459d3e
Cathy Garcia <garciacathy67@yahoo.com>
2024-01-18 22:23:48 +0000

    added search buttons

--- app-name/src/SearchBar.tsx
+++ app-name/src/SearchBar.tsx
@@ -4,6 +4,14 @@ export default function SearchBar() {
       <label className="search-bar">
         <input type="text" className="search-text" placeholder="Search..." />
       </label>
+      <div className="search-buttons-row">
+        <button type="button" className="search-buttons">
+          Search by Artist
+        </button>
+        <button type="button" className="search-buttons">
+          Search by Genre
+        </button>
+      </div>
     </>
   );
 }
