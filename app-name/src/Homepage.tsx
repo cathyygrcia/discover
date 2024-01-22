0cc9e642a8452cd12c7615db81a25d6b36e312d7
Cathy Garcia <garciacathy67@yahoo.com>
2024-01-22 21:31:07 +0000

    working on the fetch call

--- app-name/src/Homepage.tsx
+++ app-name/src/Homepage.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import SearchBar from './SearchBar';
 
 export default function Homepage() {
+  const [artist, setArtists] = useState();
+
+  console.log(artist);
+  useEffect(() => {
+    async function getRandom() {
+      try {
+        const res = await fetch(
+          'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&genreId=&apikey=aeMvG0zyzdpO1jAkGyCZeGxxQK4vIfpe',
+        );
+        if (!res.ok) throw new Error(`Error: ${res.status}`);
+        const result = await res.json();
+        setArtists(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getRandom();
+  }, []);
+
   return (
     <>
       <Header text="Discover Me" section="" />
@@ -11,16 +31,20 @@ export default function Homepage() {
           <div className="image-wrapper">
             <img src="/images/hirie.jpg" alt="hirie" className="img" />
           </div>
+          <p className="artist-name">Hirie</p>
+          <p className="artist-name">The Roxy</p>
         </div>
         <div className="artist-info">
           <div className="image-wrapper">
             <img src="/images/hirie.jpg" alt="hirie" className="img" />
           </div>
+          <p className="artist-name">Hirie</p>
         </div>
         <div className="artist-info">
           <div className="image-wrapper">
             <img src="/images/hirie.jpg" alt="hirie" className="img" />
           </div>
+          <p className="artist-name">Hirie</p>
         </div>
       </div>
       <div className="row">
@@ -28,16 +52,19 @@ export default function Homepage() {
           <div className="image-wrapper">
             <img src="/images/hirie.jpg" alt="hirie" className="img" />
           </div>
+          <p className="artist-name">Hirie</p>
         </div>
         <div className="artist-info">
           <div className="image-wrapper">
             <img src="/images/hirie.jpg" alt="hirie" className="img" />
           </div>
+          <p className="artist-name">Hirie</p>
         </div>
         <div className="artist-info">
           <div className="image-wrapper">
             <img src="/images/hirie.jpg" alt="hirie" className="img" />
           </div>
+          <p className="artist-name">Hirie</p>
         </div>
       </div>
     </>
