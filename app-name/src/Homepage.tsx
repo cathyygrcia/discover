ac9cf37a571d059090313feec9bb09a7930968bf
Cathy Garcia <garciacathy67@yahoo.com>
2024-01-22 19:50:46 +0000

    Working on artist info layout for homepage

--- app-name/src/Homepage.tsx
+++ app-name/src/Homepage.tsx
@@ -6,6 +6,40 @@ export default function Homepage() {
     <>
       <Header text="Discover Me" section="" />
       <SearchBar />
+      <div className="row">
+        <div className="artist-info">
+          <div className="image-wrapper">
+            <img src="/images/hirie.jpg" alt="hirie" className="img" />
+          </div>
+        </div>
+        <div className="artist-info">
+          <div className="image-wrapper">
+            <img src="/images/hirie.jpg" alt="hirie" className="img" />
+          </div>
+        </div>
+        <div className="artist-info">
+          <div className="image-wrapper">
+            <img src="/images/hirie.jpg" alt="hirie" className="img" />
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="artist-info">
+          <div className="image-wrapper">
+            <img src="/images/hirie.jpg" alt="hirie" className="img" />
+          </div>
+        </div>
+        <div className="artist-info">
+          <div className="image-wrapper">
+            <img src="/images/hirie.jpg" alt="hirie" className="img" />
+          </div>
+        </div>
+        <div className="artist-info">
+          <div className="image-wrapper">
+            <img src="/images/hirie.jpg" alt="hirie" className="img" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
