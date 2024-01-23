7a8e41a2e3524ff3c425e2bd6d194bc71ea6079d
Cathy Garcia <garciacathy67@yahoo.com>
2024-01-23 17:44:07 +0000

    So happy because I figured out how to pull the images! I was really struggling yesterday.

--- app-name/src/Homepage.tsx
+++ app-name/src/Homepage.tsx
@@ -5,10 +5,8 @@ import { ArtistProps } from './types';
 
 export default function Homepage() {
   const [artists, setArtists] = useState<ArtistProps[]>([]);
-  const [images, setImages] = useState<ArtistProps[]>([]);
 
   console.log(artists);
-  console.log(images);
 
   useEffect(() => {
     async function getRandom() {
@@ -19,7 +17,6 @@ export default function Homepage() {
         if (!res.ok) throw new Error(`Error: ${res.status}`);
         const result = await res.json();
         setArtists(result._embedded.events);
-        setImages(result._embedded.events.images[0].url);
       } catch (error) {
         console.error(error);
       }
@@ -35,9 +32,11 @@ export default function Homepage() {
         {artists.map((artist) => (
           <div key={artist.id} className="artist-info">
             <div className="image-wrapper">
-              {images.map((images) => (
-                <img src={images.images} alt={artist.name} className="img" />
-              ))}
+              <img
+                src={artist.images[0].url}
+                alt={artist.name}
+                className="img"
+              />
             </div>
             <p className="artist-name">{artist.name}</p>
             <p className="artist_name">{artist.venue}</p>
--- app-name/src/types.ts
+++ app-name/src/types.ts
@@ -2,5 +2,5 @@ export type ArtistProps = {
   name: string;
   id: number;
   venue: string;
-  images: string;
+  images: { url: string }[];
 };
